9dc21f97ae026432d4dd267e9ae5de4ac2f79ee3
Esteban Ibañez <e.ibannez.p@gmail.com>
2021-06-03 23:50:01 -0400

    Desafio 8 V2 done.

--- desafios/desafio-8/controllers/productos.controller.js
+++ desafios/desafio-8/controllers/productos.controller.js
@@ -1,52 +1,86 @@
-const Productos = require("../models/producto.model");
+class Productos {
+  productos = [
+    {
+      title: "Escuadra",
+      price: 345.67,
+      thumbnail:
+        "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
+      id: 1,
+    },
+    {
+      title: "test 2",
+      price: 345.6,
+      thumbnail:
+        "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-test.png",
+      id: 2,
+    },
+  ];
 
-exports.getProductos = (req, res) => {
-  console.log(Productos.getProductos());
-  try {
-    res.json({
-      productos: Productos.getProductos(),
+  constructor() {}
+
+  //metodo que agrega 1 producto al array
+  guardar(title, price, thumbnail, id) {
+    this.productos.push({
+      title: title,
+      price: price,
+      thumbnail: thumbnail,
+      id: id,
     });
-  } catch (error) {
-    console.log(`Ocurrió un error ${error}`);
-    res.json({
-      error: error,
-    });
+    return this.productos[this.productos.length - 1];
   }
-};
 
-exports.getProductoById = (req, res) => {
-  let productos = Productos.getProductos();
-  let producto = productos.filter((res) => res.id == req.params.id);
-  console.log(producto);
-  try {
-    if (producto.length == 0) {
-      res.json({
-        mensaje: `El producto con el id: ${req.params.id} no existe.`,
+  //recibe un objeto producto y lo guarda
+  postProduct = (req, res) => {
+    try {
+      debugger;
+      const { title, price, thumbnail } = req.body;
+      const id = this.productos.length + 1;
+
+      let productoAgregado = this.guardar(title, price, thumbnail, id);
+
+      console.log("producto agregado ");
+
+      return res.json({
+        mensaje: `Producto agregado con éxito!`,
+        producto: productoAgregado,
+      });
+    } catch (err) {
+      return res.json({
+        error: err.message,
       });
     }
-    res.json({
-      producto,
-    });
-  } catch (error) {}
-};
+  };
 
-exports.postProduct = (req, res) => {
-  try {
-    debugger;
-    const { title, price, thumbnail } = req.body;
-    const id = Productos.getProductos().length + 1;
+  //obtiene todos los productos por id
+  getProductoById = (req, res) => {
+    let producto = this.productos.filter((res) => res.id == req.params.id);
+    console.log(producto);
+    try {
+      if (producto.length == 0) {
+        res.json({
+          mensaje: `El producto con el id: ${req.params.id} no existe.`,
+        });
+      }
+      res.json({
+        producto,
+      });
+    } catch (error) {}
+  };
 
-    let productoAgregado = Productos.guardar(title, price, thumbnail, id);
+  //obtiene todos los productos
+  getProductos = (req, res) => {
+    console.log(this.productos);
+    try {
+      res.json({
+        productos: this.productos,
+      });
+    } catch (error) {
+      console.log(`Ocurrió un error ${error}`);
+      res.json({
+        error: error,
+      });
+    }
+  };
+}
 
-    console.log("producto agregado ");
-
-    return res.json({
-      mensaje: `Producto agregado con éxito!`,
-      producto: productoAgregado,
-    });
-  } catch (err) {
-    return res.json({
-      error: err.message,
-    });
-  }
-};
+module.exports = new Productos();
--- desafios/desafio-8/server.js
+++ desafios/desafio-8/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const handlebars = require("express-handlebars");
 const PORT = 3000;
 const app = express();
 
@@ -9,11 +9,26 @@ const ProductosRouter = require("./routers/productos.router");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+//handlebars
+app.engine(
+  "hbs",
+  handlebars( {
+    extname: ".hbs",
+    defaultLayout: "index.hbs",
+    layoutsDir: `${__dirname}/views/layouts`,
+    partialsDir: `${__dirname}/views/partials`,
+  }),
+);
+//se establece el motor de plantilla
+app.set("view engine", "hbs");
+app.set("views", "/views");
 
 //Seteo Rutas Producto
 app.use("/api/productos/listar", ProductosRouter);
 app.use("/api/productos/guardar", ProductosRouter);
+
+//se establece ruta que expone archivos html , css, js
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`servidor [DESAFIO 8] en puerto : http://localhost:${PORT}`);
